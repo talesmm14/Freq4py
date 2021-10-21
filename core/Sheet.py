b1ef421a377a9f4a541acc27b76afb7045871403
tales <tales.melquiades@ati.to.gov.br>
2021-10-21 14:53:13 -0300

    update Sheet funcions

--- core/Sheet.py
+++ core/Sheet.py
@@ -7,101 +7,73 @@ locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
 
 
 class Sheet():
-    def __init__(self, path, save_path, year, month):
+    def __init__(self, path, save_path, year, month, key_words):
         self.doc = docx.Document(path)
         self.path = path
         self.save_path = save_path
         self.year = year
         self.month = month
+        self.key_words = key_words
         self.month_days = monthrange(year, month)[1]
         self.year_month_title = (datetime.date(
             year, month, 1).strftime('%B')).swapcase() + "/" + str(year)
         self.key_table = {
-            "AS": "",
+            "AM1": "",
+            "AM2": "",
+            "AV1": "",
+            "AV2": "",
             "HM1": "08:00",
             "HM2": "14:00",
             "HV1": "12:00",
             "HV2": "18:00",
         }
 
-    def replace_keys(self, key_words):
-        for variable_key, variable_value in key_words.items():
+    def replace_key_table(self, key_table):
+        self.key_table = key_table
+
+    def __replace(self):
+        day = 0
+        weekday = 0
+        for variable_key, variable_value in self.key_words.items():
             for paragraph in self.doc.paragraphs:
-                self.replace_text_in_paragraph(
+                self.__replace_text_in_paragraph(
                     paragraph, variable_key, variable_value)
 
             for table in self.doc.tables:
-                for col in table.columns:
-                    for cell in col.cells:
+                for row in table.rows:
+                    for cell in row.cells:
+                        if cell.text == "HM1":
+                            day += 1
+                            if day <= self.month_days:
+                                weekday = date(
+                                    year=self.year, month=self.month, day=day).weekday()
+
+                        for key, value in self.key_table.items():
+                            if weekday > 5 and weekday < 6:
+                                value = self.__weekend_value()
+                            self.__replace_text_in_paragraph(
+                                paragraph, key, value)
+
                         for paragraph in cell.paragraphs:
-                            self.replace_text_in_paragraph(
+                            self.__replace_text_in_paragraph(
                                 paragraph, variable_key, variable_value)
 
-    def replace_text_in_paragraph(paragraph, key, value):
+    def __weekend_value(weekday, key):
+        if key[0] != 'H':
+            if weekday == 5:
+                return "SÁBADO"
+            if weekday == 6:
+                return "DOMINGO"
+        return "..."
+
+    def __replace_text_in_paragraph(paragraph, key, value):
         if key in paragraph.text:
             inline = paragraph.runs
             for item in inline:
                 if key in item.text:
                     item.text = item.text.replace(key, value)
 
-    def replace_table_values(self):
-        day = 0
-        for table in self.doc.tables:
-            for row in table.rows:
-                for cell in row.cells:
-                    if cell.text == "HM1":
-                        for paragraph in cell.paragraphs:
-                            day += 1
-                            if day <= self.month_days:
-                                weekday = date(
-                                    year=self.year, month=self.month, day=day).weekday()
-                            paragraph.text = self.key_table["HM1"]
-
-                    if cell.text == "AM1":
-                        for paragraph in cell.paragraphs:
-                            replace_text_in_paragraph_in_table(
-                                paragraph, "AS", weekday)
-
-                    if cell.text == "HM2":
-                        for paragraph in cell.paragraphs:
-                            paragraph.text = self.key_table["HM2"]
-
-                    if cell.text == "AM2":
-                        for paragraph in cell.paragraphs:
-                            replace_text_in_paragraph_in_table(
-                                paragraph, "AS", weekday)
-
-                    if cell.text == "HV1":
-                        for paragraph in cell.paragraphs:
-                            paragraph.text = self.key_table["HV1"]
-
-                    if cell.text == "AV1":
-                        for paragraph in cell.paragraphs:
-                            replace_text_in_paragraph_in_table(
-                                paragraph, "AS", weekday)
-
-                    if cell.text == "HV2":
-                        for paragraph in cell.paragraphs:
-                            paragraph.text = self.key_table["HV2"]
-
-                    if cell.text == "AV2":
-                        for paragraph in cell.paragraphs:
-                            replace_text_in_paragraph_in_table(
-                                paragraph, "AS", weekday)
-
-
-    def replace_text_in_paragraph_in_table(self, paragraph, key, weekday):
-        if weekday == 5:
-            paragraph.text = "SÁBADO"
-            return
-
-        if weekday == 6:
-            paragraph.text = "DOMINGO"
-            return
-
-        paragraph.text = self.key_table[key]
-
-    def save(self, key_words):
-        self.replace_keys(self, key_words)
-        self.replace_table_values(self)
-        self.doc.save(self.save_path + '/Frequência_{}.docx'.format())
+    def save(self):
+        self.__replace()
+        self.doc.save(
+            self.save_path + '/Frequência_{}.docx'.format(key_words['${FIELD_VALUE_1}']))
