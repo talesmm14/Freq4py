bf7bd7e876b5efe4905a9c95bac801779373e6f5
tales <tales.melquiades@ati.to.gov.br>
2022-01-31 14:03:27 -0300

    Implemented documentation with swagger

--- back-end/main/urls.py
+++ back-end/main/urls.py
@@ -15,13 +15,23 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('openapi/', get_schema_view(
+        title="Freq Service",
+        description="API"
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='documentation.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
 
 if settings.DEBUG:
