408ac775429f3cc91b79e8b6e49a985de02090a9
Gustavo Ferreira Parro <gustavo.ferreira.parro@gmail.com>
2022-02-07 15:04:00 -0300

    Created Login module.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(module => module.LoginModule)
   },
   {
     path: 'admin',
