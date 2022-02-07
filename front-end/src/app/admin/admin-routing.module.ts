036f9bf52adcf1bd2c88e080ab7b1b80565e836b
Gustavo Ferreira Parro <gustavo.ferreira.parro@gmail.com>
2022-02-07 14:57:16 -0300

    Removed auth module to fix error.

--- front-end/src/app/admin/admin-routing.module.ts
+++ front-end/src/app/admin/admin-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
-  { path: '',
-  component: AdminComponent
-}
+  {
+    path: '',
+    component: AdminComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
